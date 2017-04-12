87e7eb30e50f4f5f84a64f988c5935af7cca6564
KyleHoobler <Kyle.hoobler.15@cnu.edu>
2017-04-12 16:54:16 -0400

    changed back to broadcast reciever

--- app/src/main/java/com/kyle/project5/MainActivity.java
+++ app/src/main/java/com/kyle/project5/MainActivity.java
@@ -6,20 +6,37 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     BroadcastReceiver receiver;
     IntentFilter filter;
+    private int eggCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         filter = new IntentFilter();
-        receiver = new Receiver() {
 
+        receiver = new BroadcastReceiver(){
+
+
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                String action = intent.getAction();
+                if(action != null){
+
+                    
+
+                }
+
+            }
         };
+
+        registerReceiver(receiver, filter);
     }
 
     @Override
@@ -33,4 +50,25 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         unregisterReceiver(receiver);
     }
+
+    public void addOne(View view){
+        Intent i = new Intent("One Added!");
+        sendBroadcast(i);
+        eggCount++;
+
+    }
+
+    public void addTwo(View view){
+
+    }
+
+    public void minusOne(View view){
+
+    }
+
+    public void breakfast(View view){
+
+    }
+
+
 }
