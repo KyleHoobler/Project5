31349d8732f7b63b78195fc1e627b0efa8ee160f
KyleHoobler <Kyle.hoobler.15@cnu.edu>
2017-04-12 16:26:29 -0400

    Reciever

---
+++ app/src/main/java/com/kyle/project5/Reciever.java
@@ -0,0 +1,15 @@
+package com.kyle.project5;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Kyle on 4/12/2017.
+ */
+public class Reciever extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        
+    }
+}
